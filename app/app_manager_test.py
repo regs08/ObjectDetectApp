e4d7dab087565d9e683cb10d60b0831b07d16406
cregs08 <cole.regnier@gmail.com>
2025-01-15 14:58:27 -0500

    filepath update

--- app/app_manager_test.py
+++ app/app_manager_test.py
@@ -1,6 +1,6 @@
 from app.app_manager import AppManager
 
-config_file = "/Users/cole/PycharmProjects/liteRT_test/assets/configs/app_manager.yaml"
+config_file = "/Users/cole/PycharmProjects/ObjectDectectApp/assets/configs/app_manager.yaml"
 app_manager = AppManager()
 app_manager.initialize(config_file)
 app_manager.run()
