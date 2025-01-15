82e42ba72231e63ce717b2e0cca90a3068ef0561
cregs08 <cole.regnier@gmail.com>
2025-01-15 15:02:47 -0500

    logic overhaul: putting more control into the manager. all processing, annotations

--- model_logic/base_classes/model_manager.py
+++ model_logic/base_classes/model_manager.py
@@ -1,69 +1,72 @@
-from annotator.detections.detections_annotator import FrameAnnotatorDetections
 from app.base_classes.manager import BaseManager
-from model_logic.yolo.model.yolo_ncnn_model import YoloNcnnModel
 from utils.configs.config_base import Config
-from model_logic.yolo.model.yolo_model import YOLOModel
 import numpy as np
 
 from utils.data_package.yolo_det_data_package import YoloDetectionDataPackage
+from utils.factories.model_factory import ModelFactory
+from utils.factories.processor_factory import ProcessorFactory
 
 
 class ModelManager(BaseManager):
     def __init__(self):
         super().__init__()
 
-        self.model_path = None
-        self.class_labels = None
         self.preprocessor = None
         self.postprocessor = None
         self.annotator = None
-        self.model_factory = {'YoloModel': YOLOModel,
-                              'YoloNCNNModel': YoloNcnnModel,}
-        self.annotator_factory = {'DetectionAnnotator': FrameAnnotatorDetections}
+        self.model_factory = ModelFactory()
+        self.processor_factory = ProcessorFactory()
+        self.model_config = None
         self.confidence_threshold = None
         self.model = None
+        self.model_config_path = None
 
-    def initialize(self, *args):
+    def initialize(self, config: Config):
         """
         intialize the model's compenentents, pre/post processor
-        :param args:
+        :param config:
         :return:
         """
-        pass
+        self.apply_config(config)
+        self.model.initialize(config=self.model_config)
+        self.preprocessor.initialize()
+        self.postprocessor.initialize(self.model_config.get("class_labels"))
+        self.annotator.initialize()
 
-    # def set_pre_post_processors(self, *args):
-    #     raise NotImplementedError ("must implement set post and preprocessor")
 
-    def populate_with_config(self, config: Config):
-        """
-        Intialize ModelManager with specific configurations.
-        """
-        # Create model instance dynamically (using a factory or import logic)
-        self.model = self.model_factory[config.get('model')]()
-        self.model_path = config.get("model_path")
-        self.confidence_threshold =config.get("confidence_threshold")
-        self.class_labels = config.get("class_labels")
-        self.annotator = self.annotator_factory[config.get("annotator")]()
+    def apply_config(self, config: Config):
+        self.model_config_path = config.get("model_config_path")
+        # Creating Config object to be passed into our model object
+        self.model_config = self.config_manager.create_config_object(config_path=self.model_config_path)
+        # Getting type of model from config
+        self.model = self.model_factory.models[self.model_config.get("type")]()
+        # setting pre and post processors
+        # For now the prepocessing is handled by ultralytics. Delete if we don't need or only working with ultra
+        self.preprocessor = self.processor_factory.processors[self.model_config.get("preprocessor")]()
+        self.postprocessor = self.processor_factory.processors[self.model_config.get("postprocessor")]()
+        # setting annotator
+        self.annotator = self.annotator_factory.annotators[self.model_config.get("annotator")]()
 
     def set_name(self):
         self.name = "ModelManager"
-
-    def utils_defined(self):
-        if self.preprocessor is None or self.postprocessor is None or self.annotator is None:
-            return False
-        else:
-            return True
 
     def run(self, frame: np.ndarray):
         """
         Main thread logic for processing frames.
         """
         try:
+            # preprocess frame
+            preprocessed_frame = self.preprocessor.preprocess_image(frame)
             #run inference
-            detections = self.model.predict(frame)
+            results = self.model.predict(preprocessed_frame)
             #annotate frame
+            # postprocess Frame
+            detections = self.postprocessor.postprocess(results[0])
             # if no detections just the orig frame is returned
-            annotated_frame = self.annotator.annotate_frame(detections, frame)
+            annotated_frame = self.annotator.annotate_frame(scene=frame,
+                                                            detections=detections,
+                                                            labels=self.model.labels)
+
             #create data package
             data_package = YoloDetectionDataPackage(detections=detections,
                                        frame=annotated_frame)
