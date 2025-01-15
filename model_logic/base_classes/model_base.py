a4cdfb3d4b7cecebe6731e52897f9db6afb5bd26
cregs08 <cole.regnier@gmail.com>
2025-01-15 15:00:21 -0500

    changing base sigs, getting rid of the preprocessor and labels/model path

--- model_logic/base_classes/model_base.py
+++ model_logic/base_classes/model_base.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 class ModelBase(ABC):
     def __init__(self,):
-        self.preprocessor = None
-        self.postprocessor = None
+        self.labels = None
+        self.model_path = None
         self.config = None
-
+        self.confidence_threshold = None
     @abstractmethod
     def initialize(self, *args):
         pass
@@ -17,3 +17,21 @@ class ModelBase(ABC):
         :return: Predictions in human-readable format.
         """
         pass
+    @staticmethod
+    def load_class_labels(source):
+        """
+        Load class labels from a file or directly from a list.
+        :param source: Either a path to a .txt file or a list of class labels.
+        :return: List of class labels.
+        """
+        if isinstance(source, str):  # If a file path is provided
+            if not source.endswith(".txt"):
+                raise ValueError("Unsupported file format. Only .txt files are supported.")
+            with open(source, "r") as f:
+                class_labels = [line.strip() for line in f.readlines()]
+        elif isinstance(source, list):  # If a list of labels is provided directly
+            class_labels = source
+        else:
+            raise ValueError("Source must be a file path (str) or a list of labels.")
+
+        self.class_labels = class_labels
