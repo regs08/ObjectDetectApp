de659547c5f7a7ce4c8e23fd54066a6e37fd2028
cregs08 <cole.regnier@gmail.com>
2025-01-26 12:49:56 -0500

    more accutrate config

--- model_logic/yolo/model/yolo_ncnn_model.py
+++ model_logic/yolo/model/yolo_ncnn_model.py
@@ -1,4 +1,3 @@
-from annotator.frame_annotator_base import FrameAnnotatorBase
 from model_logic.base_classes.model_base import ModelBase
 from utils.configs.config_base import Config
 from ultralytics import YOLO
@@ -8,13 +7,15 @@ class YoloNcnnModel(ModelBase):
     def __init__(self):
         super().__init__()
         self.model = None
+        self.task = None
     def initialize(self, config: Config):
         self.model_path = config.get("model_path")
         self.labels = config.get("labels")
+        self.task = config.get("task")
         self.confidence_threshold = config.get("confidence_threshold")
-        self.model = YOLO(self.model_path)
+        self.model = YOLO(self.model_path, task=self.task)
     def predict(self, image):
-        results = self.model(image)
+        results = self.model(image, verbose=False)
         return results
 
 
